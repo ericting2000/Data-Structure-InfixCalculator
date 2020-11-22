f7c4304b21ed9283cd29396bb1bed659cb6f80f3
ericting2000 <erictingting2000@gmail.com>
2020-11-22 12:45:49 +0800

    create stack.c

---
+++ stack.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+void init_stack(stack *s){
+    s->top = 0;
+}
+
+int stack_full(stack *s){
+    return (s->top >= 100);
+}
+
+int stack_empty(stack *s){
+    return (s->top <= 0);
+}
+
+void push(stack *s, char i){
+    if (stack_full(s)) {
+        printf("stack is full\n");
+        return;
+    }
+    s->elements[s->top] = i;
+    s->top++;
+}
+
+int pop_stack(stack *s){
+    if (stack_empty(s)) {
+        printf("stack is empty\n");
+        return (-1);
+    }
+    s->top--;
+    return (s->elements[s->top]);
+}
